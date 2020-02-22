83a8e4fdd7e77fe1757d198075f4f35e3c6284aa
Jacopo <32840785+jjak0b@users.noreply.github.com>
2020-02-22 21:56:12 +0100

    Aggiunta inizializzazione a 0 stato del processore

--- src/pcb/pcb.c
+++ src/pcb/pcb.c
@@ -110,6 +110,42 @@ pcb_t *allocPcb(void){
         new->p_parent = NULL;
         new->p_semkey = NULL;
         new->priority = 0;
+#ifdef TARGET_UMPS
+        new->p_s.entry_hi = 0;
+        new->p_s.cause = 0;
+        new->p_s.status = 0;
+        new->p_s.pc_epc = 0;
+        new->p_s.hi = 0;
+        new->p_s.lo = 0;
+        int i;
+        for( i = 0; i < STATE_GPR_LEN; i++ ){
+            new->p_s.gpr[ i ] = 0;
+        }
+#endif
+#ifdef TARGET_UARM
+        new->p_s.a1 = 0;
+        new->p_s.a2 = 0;
+        new->p_s.a3 = 0;
+        new->p_s.a4 = 0;
+        new->p_s.v1 = 0;
+        new->p_s.v2 = 0;
+        new->p_s.v3 = 0;
+        new->p_s.v4 = 0;
+        new->p_s.v5 = 0;
+        new->p_s.v6 = 0;
+        new->p_s.sl = 0;
+        new->p_s.fp = 0;
+        new->p_s.ip = 0;
+        new->p_s.sp = 0;
+        new->p_s.lr = 0;
+        new->p_s.pc = 0;
+        new->p_s.cpsr = 0;
+        new->p_s.CP15_Control = 0;
+        new->p_s.CP15_EntryHi = 0;
+        new->p_s.CP15_Cause = 0;
+        new->p_s.TOD_Hi = 0;
+        new->p_s.TOD_Low = 0;
+#endif
     }
     return new;
 }
