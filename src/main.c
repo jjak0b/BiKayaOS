44cdbe36830f5a81710024a987d3515fcace8fb4
Stefano De Santis <stefano.desantis4@studio.unibo.it>
2020-03-18 09:42:04 +0000

    [SDS-002][SDS-003] Aggiunto qualche commento + initPcbs e inizializzazione ready queue; #2 #3

--- src/main.c
+++ src/main.c
@@ -1,7 +1,37 @@
+/***********************************main.c****************************************
+*                   - Bikaya OS - Phase 1.5 - Main -
+*    
+*	Welcome to BiKaya OS!
+*	To test kernel, just read README.md 
+*	Enjoy using BiKaya OS. :)
+*
+*	Copyright (c) 2020 lso20az15. All rights reserved.
+*	This work is licensed under the terms of the MIT license.
+*	For a copy, see LICENSE.
+* 	 
+*	@credit: 
+*   Stefano De Santis, Cristiano Guidotti, Iacopo Porcedda, Jacopo Rimediotti
+*/
+
 #include "system.h"
+#include "pcb/pcb.h"
+
+struct list_head *ready_queue;
 
 void main(){
 	// Inizialializzazione del sistema
 	//----------------------------------------------------
 	initAreas();
+	//----------------------------------------------------
+
+	initPcbs();
+	INIT_LIST_HEAD(ready_queue);
+
+	// TODO:
+	//----------------------------------------------------
+
+
+	// Halt
+	//----------------------------------------------------
+	HALT();
 }
