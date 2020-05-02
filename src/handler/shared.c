84e873db15a697ce52d8c20c199a2fccff1d1cd0
cguidotti <46874835+cguidotti@users.noreply.github.com>
2020-05-02 10:03:26 +0200

    aggiunte sys2 e sys3

--- src/handler/shared.c
+++ src/handler/shared.c
@@ -22,7 +22,48 @@
 #include <handler/shared.h>
 #include <scheduler/scheduler.h>
 
-void sys3_terminate(void){
-    scheduler_StateToTerminate(TRUE);
-    scheduler_StateToRunning();
+int sys2_create( state_t *child_state, int child_priority, void **child_pid ) {
+  *child_pid = allocPCB(); /* il puntatore sarà nullo in caso non vi siano PCB disponibili */
+
+  if ( *child_pid ) {
+    (*child_pid)->p_s = *child_state;
+    (*child_pid)->priority = child_priority;
+    (*child_pid)->original_priority = child_priority;
+    insertChild( scheduler_GetRunningProcess(), *child_pid );
+    scheduler_AddProcess( *child_pid );
+    return 0;
+  }
+  else
+    return (-1);
+}
+
+int sys3_terminate( void *pid ) {
+  if ( pid ) {
+    if ( !(scheduler_FindReadyProc( pid )) )
+      return (-1); /* se il PCB non esiste ritorna errore */
+  }
+  else
+    pid = scheduler_GetRunningProcess();
+
+  /* ogni processo figlio viene rimosso da un'eventuale queue di semaforo */
+  struct list_head *child_iter;
+  pcb_t *dummy;
+
+  list_for_each( child_iter, &pid->p_child ) {
+    dummy = container_of( child_iter, pcb_t, p_sib );
+    int *key = dummy->p_semkey;
+    if ( !(key) ) {
+      list_del( &dummy->p_next ); /* rimozione del PCB dalla coda dei processi bloccati */
+
+      /* aumento del valore del semaforo. se positivo si risveglia il primo in coda */
+      *key += 1;
+      if ( *key > 0 )
+        scheduler_AddProcess( removeBlocked( key ) );
+    }
+  }
+
+  scheduler_StateToTerminate( TRUE );
+  scheduler_StateToRunning();
+
+  return 0;
 }
